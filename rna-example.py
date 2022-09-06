001d0136327daa1bc3e7dc5210bdbc6950ea288e
DanieleMarchei <danielemarchei@gmail.com>
2022-09-06 17:49:17 +0200

    plotting barcodes

--- rna-example.py
+++ rna-example.py
@@ -1,14 +1,17 @@
 import bettifold as bf
 import matplotlib.pyplot as plt
-from ripser import Rips
+from barcodes import Barcode
 
 output = bf.bettifold("UCCACAGGCAG",
                        # remember to specify which type of bonds are allowed
                        bond_constraints = [bf.watson_creek, bf.min_bond_len(4)],
                        folding_constraints = [bf.min_bonds(2), bf.max_bonds(5)],
                        folder = "rna-test",
-                       maxdim = 2
+                       maxdim = 1
                        )
 
-output["rips"].plot()
+rips = output["rips"]
+rips.plot()
 plt.show()
+br = Barcode(rips)
+br.plot_barcode()
